d5df16be32fd2d2a4ca610ccccc925dd43c01a06
Jai Gokhale <gokhale.jaidev@gmail.com>
2020-10-09 15:12:20 -0500

    added bloomer styles

--- src/App.js
+++ src/App.js
@@ -1,32 +1,33 @@
-import React, {Component} from "react";
-import RandomUser from './components/RandomUser.jsx';
-
+import React, { Component } from "react";
+import { Container, Button  } from 'bloomer';
+import "bulma/css/bulma.css"
 import './App.css';
+import RandomUser from './components/RandomUser.jsx';
 
 class App extends Component {
   //loading data into state
   state = {
     userData: [],
-    
+
   };
-  
-//Helper function to fetch data from API
-loadData = async () => {
-  const response = await fetch('https://randomuser.me/api/?results=10');
-  const data = await response.json();
-  return data;
-};
- handleClick = async () => {
-  const userData = await this.loadData();
-  this.setState({
-    userData: userData.results
-  });
-}
-//Built is React method that acts like DOM content loaded event listener, it is listening for this PIECE of the dom is loaded (lifecycle method)
-  async componentDidMount(){
+
+  //Helper function to fetch data from API
+  loadData = async () => {
+    const response = await fetch('https://randomuser.me/api/?results=10');
+    const data = await response.json();
+    return data;
+  };
+  handleClick = async () => {
+    const userData = await this.loadData();
+    this.setState({
+      userData: userData.results
+    });
+  }
+  //Built is React method that acts like DOM content loaded event listener, it is listening for this PIECE of the dom is loaded (lifecycle method)
+  async componentDidMount() {
     console.log("The component mounted");
     const userData = await this.loadData();
-    
+
     this.setState({
       userData: userData.results
     });
@@ -35,29 +36,32 @@ loadData = async () => {
 
 
 
-  render(){
-     //let RenderUser;
+  render() {
+    //let RenderUser;
     //if array has length
     // if(this.state.userData.length){
     //   RenderUser = <RandomUser userData={this.state.userData[0]}/>;
     // }else{
     //   RenderUser = <p>no user data</p>;
     // }
-    const {userData} = this.state;
+    const { userData } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-         <h1>Random Users</h1>
-        </header>
-        <button onClick={this.handleClick}>Load More Users</button>
-        {userData.length ? ( <RandomUser userData={userData}/>) : <p>no user data</p> }
-        {/* {RenderUser} */}
-        
+        <Container is-fluid>
+          <header className="App-header">
+            <h1>Random Users</h1>
+          </header>
+          <Button isHovered='false' className="search-button" onClick={this.handleClick}>Load More Users</Button>
+          {userData.length ? (<RandomUser userData={userData} />) : <p>no user data</p>}
+          {/* {RenderUser} */}
+
+        </Container>
+
       </div>
       //^ passing data from parent to child as a prop
     );
   }
- 
+
 }
 
 export default App;
--- src/components/RandomUser.jsx
+++ src/components/RandomUser.jsx
@@ -1,25 +1,41 @@
 import React from "react";
+
+import { Columns, Column, Card, CardImage, Image, Title, Content, withHelpersModifiers } from 'bloomer';
+import "bulma/css/bulma.css"
 import "./RandomUser.css";
 
 //refactored as a function
 function RandomUser(props) {
     //passing data to the component via props
     const { userData } = props;
+    console.log(userData)
     //accessing the props
     return (
-        <div className="container">
-        <ul>
-            {userData.map((user)=>(
+
+        <div className="wrapper">
+            {userData.map((user) => (
                 //every li should have a key if you map through data in React
-                <li className="user-card" key={user.login.uuid}>
-                    <img src={user.picture.large} alt={`${user.name.first} ${user.name.last}`}></img>
-                    <p>{user.name.first} {user.name.last}</p>
-                    <p>{user.email}</p>
-                    <p>{user.cell}</p>
-                </li>
-            ))}     
-        </ul>
+
+                <Card className="card" key={user.login.uuid}>
+                    <CardImage>
+                        <Image isSize='128x128' src={user.picture.large} />
+                    </CardImage>
+                    <Title hasTextAlign='left' isSize={4}>{user.name.first} {user.name.last}</Title>
+                    <Content hasTextAlign='left'>
+                        {user.email}
+                    </Content>
+                    <Content hasTextAlign='left'>
+                        {user.cell}
+                    </Content>
+                </Card>
+
+
+            ))}
         </div>
+
+
+
+
 
 
     );
